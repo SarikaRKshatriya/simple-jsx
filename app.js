9c765dd74cdf3dd497456dbf79e0c9d160f12247
Sarika Kshatriya <46900849+SarikaRKshatriya@users.noreply.github.com>
2024-11-21 11:15:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = <h1>JSX</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
